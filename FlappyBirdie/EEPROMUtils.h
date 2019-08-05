c66926a8c10083f66a3943d3cca5735dc8188322
Pharap <2933055+Pharap@users.noreply.github.com>
2019-08-06 00:49:22 +0100

    Add remaining source files

---
+++ FlappyBirdie/EEPROMUtils.h
@@ -0,0 +1,17 @@
+#ifndef EEPROMUTILS_H
+#define EEPROMUTILS_H
+#define EEPROM_START_C1  EEPROM_STORAGE_SPACE_START
+#define EEPROM_START_C2  EEPROM_START_C1 + 1
+#define EEPROM_SCORE     EEPROM_START_C1 + 2
+
+void initEEPROM() {
+  unsigned char c1 = EEPROM.read(EEPROM_START_C1);
+  unsigned char c2 = EEPROM.read(EEPROM_START_C2);
+
+  if(c1 != 'L' || c2 != 'Z') {
+    EEPROM.update(EEPROM_START_C1, 'L');
+    EEPROM.update(EEPROM_START_C2, 'Z');
+    EEPROM.put(EEPROM_SCORE, (unsigned int)0);
+  }
+}
+#endif
